538af0d2ce2d93066d637a1a68f94e59f30dcd26
youssef-lahdhiri <lahdhiriyoussef0@gmail.com>
2025-02-02 12:28:13 +0100

    eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@next/next/no-html-link-for-pages': 'off',
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  }),
+]
 
 export default eslintConfig;
