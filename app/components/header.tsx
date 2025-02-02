66dedd3bc56c0f6fca5d5ce415e8adf702f8f1c5
youssef-lahdhiri <lahdhiriyoussef0@gmail.com>
2025-02-02 13:13:51 +0100

    fixed header logo

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <>
         <div className=" bg-transparent w-full  items-center  border     md:px-48 md:flex hidden  justify-between  h-16 ">
-            <p className="text-cente font-extrabold pl-10 flex items-center justify-center"><a href="/SwiftDrive"></a>RENTAL<BiCar /> </p>
+            <p className="text-cente font-extrabold pl-10 flex items-center justify-center gap-2"><a href="/SwiftDrive"></a><BiCar />RENTAL </p>
         <ul className=" font-semibold  hidden md:flex gap-4 justify-center">
         <li className="hover:text-background text-bold"> <a href="/SwiftDrive">Home</a></li>
         <li className="hover:text-background"> <a href="/SwiftDrive/map">Rent</a></li>
